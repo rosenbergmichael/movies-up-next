6c4eacd182ece23236f6312ec45a4c0f759804b4
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-26 16:54:49 -0400

    in app.js create Movie class with constructor and movie, date and location parameters

---
+++ app.js
@@ -0,0 +1,19 @@
+// Movie Class: Represents a Movie
+
+class Movie {
+  constructor(movie, date, location) {
+    this.movie = movie;
+    this.date = date;
+    this.location = location;
+  }
+}
+
+// UI Class: Handle UI Tasks
+
+// Store Class: Handles Storage
+
+// Event: Display Movies
+
+// Event: Add a Movie
+
+// Event: Remove a Movie
