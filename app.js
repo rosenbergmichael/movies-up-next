6172758897627d0f6fb01f2e00ae637dbd35304f
Michael Rosenberg <rosenbergmj1@gmail.com>
2021-03-26 20:00:29 -0400

    in app.js create UI class with static function displayMovies, variable storedMovies and variable movies

--- app.js
+++ app.js
@@ -10,6 +10,25 @@ class Movie {
 
 // UI Class: Handle UI Tasks
 
+class UI {
+  static displayMovies() {
+    const storedMovies = [
+      {
+        movie: 'Sopranos Sequel',
+        date: 'July 15 2021',
+        location: 'HBO Max'
+      },
+      {
+        movie: 'Mortal Kombat',
+        date: 'August 10 2021',
+        location: 'In Theatres'
+      }
+    ];
+
+    const movies = storedMovies;
+  }
+}
+
 // Store Class: Handles Storage
 
 // Event: Display Movies
